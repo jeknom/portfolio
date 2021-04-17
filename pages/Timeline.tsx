af463010a3869de9da82620a2675d4276f91ed86
Johannes Palvanen <palvanenj@gmail.com>
2021-04-17 12:29:38 +0300

    Fix open graph data for timeline

--- pages/Timeline.tsx
+++ pages/Timeline.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import Image from 'next/image'
-import { getAchievements, getMinAchievementDate, getHighlights } from '../lib/data'
+import { getAchievements, getMinAchievementDate, getHighlights, getOpenGraphData } from '../lib/data'
 import { Head, FlatButton } from '../components/Core'
 import TimelineComponent from '../components/Timeline'
 import styles from '../styles/Timeline.module.css'
@@ -8,15 +8,22 @@ import styles from '../styles/Timeline.module.css'
 interface TimelineProps {
   achievements: AchievementData[],
   minAchievementDate: MinAchievementDateData,
-  highlights: HighlightData[]
+  highlights: HighlightData[],
+  openGraphData: OpenGraphData
 }
 
-const Timeline: FC<TimelineProps> = ({ achievements, minAchievementDate, highlights }) => {
+const Timeline: FC<TimelineProps> = ({
+  achievements,
+  minAchievementDate,
+  highlights,
+  openGraphData }) => {
   return (
     <>
       <Head
-        title='Timeline'
-        description='The story of my career by Johannes Palvanen.' />
+        title={openGraphData.title}
+        description={openGraphData.description}
+        type={openGraphData.type}
+        imagePath={openGraphData.image} />
       <a href='/'>
         <FlatButton className={styles.closeButton}>
           <Image src='/x.svg' alt='Close button X' width='64' height='64' priority />
@@ -31,13 +38,18 @@ const Timeline: FC<TimelineProps> = ({ achievements, minAchievementDate, highlig
 }
 
 export async function getServerSideProps() {
-  const result = await Promise.all([getAchievements(), getMinAchievementDate(), getHighlights()])
+  const result = await Promise.all([
+    getAchievements(),
+    getMinAchievementDate(),
+    getHighlights(),
+    getOpenGraphData()])
   const achievements = result[0]
   const minAchievementDate = result[1]
   const highlights = result[2]
+  const openGraphData = result[3]
 
   return {
-    props: { achievements, minAchievementDate, highlights }
+    props: { achievements, minAchievementDate, highlights, openGraphData }
   }
 }
 
