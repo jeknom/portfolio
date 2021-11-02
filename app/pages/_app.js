d760c66649f0ef341935a901f4b6e9558cb5c31e
Johannes Palvanen <palvanenj@gmail.com>
2021-11-02 14:37:15 +0200

    Fix nprogress style import

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,7 +1,7 @@
 import NProgress from "nprogress";
 import Router from "next/router";
 import '../styles/globals.css';
-import '../styles/customNProgress.css';
+import '../styles/customNprogress.css';
 
 Router.events.on("routeChangeStart", NProgress.start);
 Router.events.on("routeChangeComplete", NProgress.done);
