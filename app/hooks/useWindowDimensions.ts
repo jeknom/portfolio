9ae321801997502740b85028bd0d650e6000154e
Johannes Palvanen <palvanenj@gmail.com>
2021-12-04 11:14:26 +0200

    Improve the useWindowDimensions hook

--- app/hooks/useWindowDimensions.ts
+++ app/hooks/useWindowDimensions.ts
@@ -2,40 +2,26 @@ import { useState, useEffect } from "react";
 
 // This is from here: https://stackoverflow.com/questions/36862334/get-viewport-window-height-in-reactjs
 
-function getWindowDimensions() {
-  let width = 0;
-  let height = 0;
-
-  try {
-    width = window.innerWidth;
-    height = window.innerHeight;
-  } catch (error) {
-    console.warn("Window not set!");
-  }
-
-  return { width, height };
-}
-
 export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
+  const [windowDimensions, setWindowDimensions] =
+    useState<{ width: number; height: number }>();
 
   useEffect(() => {
     function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+      let width = window.innerWidth;
+      let height = window.innerHeight;
+
+      setWindowDimensions({ width, height });
     }
 
-    try {
-      window.addEventListener("resize", handleResize);
-
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    } catch (error) {
-      console.warn("Window not set!");
+    if (!windowDimensions) {
+      handleResize();
     }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  return windowDimensions;
+  return windowDimensions || { width: 0, height: 0 };
 }
