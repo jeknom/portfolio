04a4fe15ef9e261eb75a9e1d031a01488e553596
Johannes Palvanen <palvanenj@gmail.com>
2021-12-04 17:21:28 +0200

    Fix broken imports

--- app/components/Core/index.ts
+++ app/components/Core/index.ts
@@ -27,7 +27,6 @@ import Alert from "./Alert";
 import DatePicker from "./DatePicker";
 import Sidebar from "./Sidebar";
 import Protected from "./Protected";
-import ContactInformation from "./ContactInformation";
 
 export {
   Button,
@@ -59,5 +58,4 @@ export {
   DatePicker,
   Sidebar,
   Protected,
-  ContactInformation,
 };
--- app/pages/unauthorized.tsx
+++ app/pages/unauthorized.tsx
@@ -1,4 +1,3 @@
-import { Root } from "components/Core";
 import { FC } from "react";
 
 interface UnauthorizedProps {}
